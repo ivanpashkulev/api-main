27cdfc1c71c1456816dfafebfea4d25bca1db4d6
Ivan Pashkulev <ivan.pashkulev@gmail.com>
2026-09-22 20:58:34 +0300

    Instantiated ChatService on app startup

--- src/ivanpashkulev/main.py
+++ src/ivanpashkulev/main.py
@@ -1,10 +1,24 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+from ivanpashkulev.chat.dependencies import get_chat_service
 from ivanpashkulev.chat.router import router as chat_router
 from ivanpashkulev.core.config import settings
 
-app = FastAPI(title=settings.app_name, debug=settings.debug)
+
+@asynccontextmanager
+async def lifespan(_: FastAPI):
+    get_chat_service()
+    yield
+
+
+app = FastAPI(
+    title=settings.app_name,
+    debug=settings.debug,
+    lifespan=lifespan,
+)
 
 app.add_middleware(
     CORSMiddleware,
